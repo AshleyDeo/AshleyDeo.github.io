e6163dffdd5e6bf8b3b3c16df1c7f4633a73d694
AshleyDeo <35739765+AshleyDeo@users.noreply.github.com>
2020-03-02 09:42:00 -0500

    Fixed Creative Coding site

--- Classes/CreativeCoding/hw_1.js
+++ Classes/CreativeCoding/hw_1.js
@@ -1,9 +1,12 @@
+let annoy = 0; //# of clicks on Monster
+let drawAll = 1; // draw all except face
+
 class BlanketMonster {
-    constructor(x, y) {
-        this.pos_x = x;
-        this.pos_y = y;
-        this.orig_rgb; //original color 
-        this.blanket_rgb; //color var
+    constructor(x_, y_, color_) {
+        this.pos_x = x_;
+        this.pos_y = y_;
+        this.orig_rgb = color_; //original color 
+        this.blanket_rgb = this.orig_rgb; //color var
 
         this.touch = 0; // touch bool for arm
         this.armLength = 10;
@@ -23,115 +26,115 @@ class BlanketMonster {
         strokeWeight(4);
         stroke(51);
         fill(252, 213, 114);
-        rect(pos_x + 450, pos_y + 100, armLength, 50, 30);
+        rect(this.pos_x + 450, this.pos_y + 100, this.armLength, 50, 30);
 
         //Blanket
-        fill(blanket_rgb);
+        fill(this.blanket_rgb);
         strokeWeight(4);
         stroke(51);
-        rect(pos_x + 50, pos_y, 800, 220, 20);
+        rect(this.pos_x + 50, this.pos_y, 800, 220, 20);
 
         //Bumps
         //fill(255);
         strokeWeight(5);
         stroke(51);
-        arc(pos_x + 250, pos_y + 50, 400, 350, PI, TWO_PI, OPEN);
-        arc(pos_x + 625, pos_y + 60, 450, 300, PI, TWO_PI, OPEN);
+        arc(this.pos_x + 250, this.pos_y + 50, 400, 350, PI, TWO_PI, OPEN);
+        arc(this.pos_x + 625, this.pos_y + 60, 450, 300, PI, TWO_PI, OPEN);
 
         if (pillow1.clicked == 1) {
-            blanket_rgb = color(45, 59, 252);
-            sleep();
-        } else if (anger == 1) { //click on monster
-            shake();
-        } else if (touch == 1) { //clicked on circle
-            moveArm();
+            this.blanket_rgb = color(45, 59, 252);
+            this.sleep();
+        } else if (this.anger == 1) { //click on monster
+            this.shake();
+        } else if (this.touch == 1) { //clicked on circle
+            this.moveArm();
         } else {
-            orig_rgb = color(3, 240, 252);
+            this.orig_rgb = color(3, 240, 252);
         }
 
     }
 
     reset() { //reset monster vars
-        pos_x = 400;
-        pos_y = 400;
-        blanket_rgb = orig_rgb;
-        armLength = 10;
+        this.pos_x = 400;
+        this.pos_y = 400;
+        this.blanket_rgb = this.orig_rgb;
+        this.armLength = 10;
     }
 
     sleep() {
         fill(0);
-        textSize(fontsize);
-        text("Z", text_pos_x, text_pos_y);
+        textSize(this.fontsize);
+        text("Z", this.text_pos_x, this.text_pos_y);
 
         //move text
-        text_pos_x += text_mov;
-        text_pos_y -= text_mov;
-        fontsize += 2;
+        this.text_pos_x += this.text_mov;
+        this.text_pos_y -= this.text_mov;
+        this.fontsize += 2;
 
-        if (text_pos_y < 0) { //when off screen reset text animation
-            text_pos_x = 900;
-            text_pos_y = 400;
-            fontsize = 60;
+        if (this.text_pos_y < 0) { //when off screen reset text animation
+            this.text_pos_x = 900;
+            this.text_pos_y = 400;
+            this.fontsize = 60;
         }
     }
 
     moveArm() {
-        blanket_rgb = color(252, 245, 114);
+        this.blanket_rgb = color(252, 245, 114);
 
         fill(0);
         textSize(110);
-        text("??", width / 2, 200);
+        text("??", windowWidth / 2, 200);
 
-        if (armLength >= 1000 || armBack == 1) { //when arm reaches circle come back
-            armBack = 1;
-            armLength -= 20;
+        if (this.armLength >= 1000 || this.armBack == 1) { //when arm reaches circle come back
+            this.armBack = 1;
+            this.armLength -= 20;
         } else { // move arm out
-            armLength += 10;
+            this.armLength += 10;
         }
 
-        if (armBack == 1 && armLength <= 10) { // reset arm vars
-            armLength = 10;
-            touch = 0;
-            armBack = 0;
-            reset();
+        if (this.armBack == 1 && this.armLength <= 10) { // reset arm vars
+            this.armLength = 10;
+            this.touch = 0;
+            this.armBack = 0;
+            this.reset();
         }
     }
 
     shake() {
-        blanket_rgb = color(255, 0, 0);
-        pos_x += random(-7, 7);
-        pos_y += random(-7, 7);
+        this.blanket_rgb = color(255, 0, 0);
+        this.pos_x += random(-7, 7);
+        this.pos_y += random(-7, 7);
 
         //check if monster is outside radius and redo calc
-        while (pos_x > 407 || pos_x < 393) {
-            pos_x = 400;
-            pos_x += random(-7, 7);
+        while (this.pos_x > 407 || this.pos_x < 393) {
+            this.pos_x = 400;
+            this.pos_x += random(-7, 7);
         }
 
-        while (pos_y > 407 || pos_y < 393) {
-            pos_y = 400;
-            pos_y += random(-7, 7);
+        while (this.pos_y > 407 || this.pos_y < 393) {
+            this.pos_y = 400;
+            this.pos_y += random(-7, 7);
         }
 
         //Timer til popup
         //60 fps
-        if (frameCount % 60 == 0 && annoyTimer > 0) {
-            annoyTimer--;
+        if (frameCount % 60 == 0 && this.annoyTimer > 0) {
+            this.annoyTimer--;
         }
-        if (annoy >= 3 || annoyTimer <= 0) { //call popup face
+        if (annoy >= 3 || this.annoyTimer <= 0) { //call popup face
             drawAll = 0;
             drawFace();
             //timer popup lasts
-            if (frameCount % 60 == 0 && faceTimer > 0) {
-                faceTimer--;
+            if (frameCount % 60 == 0 && this.faceTimer > 0) {
+                this.faceTimer--;
             }
-            if (faceTimer == 0) { //reset when finished
+            if (this.faceTimer == 0) { //reset when finished
                 drawAll = 1;
-                anger = 0;
+                this.anger = 0;
                 annoy = 0;
-                faceTimer = 3;
-                annoyTimer = 3;
-                reset();
+                this.faceTimer = 3;
+                this.annoyTimer = 3;
+                this.reset();
             }
         }
     }
@@ -148,14 +151,14 @@ class Pillow {
         fill(200);
         strokeWeight(4);
         stroke(51);
-        rect(pos_x, pos_y, 200, 100);
+        rect(this.pos_x, this.pos_y, 200, 100);
         strokeWeight(5);
         //fill(255);
-        arc(pos_x + 5, pos_y + 50, 50, 100, HALF_PI, HALF_PI + PI, OPEN);
+        arc(this.pos_x + 5, this.pos_y + 50, 50, 100, HALF_PI, HALF_PI + PI, OPEN);
         //arc(pos_x + 195, pos_y + 50, 50, 100, -HALF_PI, HALF_PI, OPEN);
-        ellipse(pos_x + 195, pos_y + 50, 75, 100);
+        ellipse(this.pos_x + 195, this.pos_y + 50, 75, 100);
 
-        if (clicked == 1) {
+        if (this.clicked == 1) {
             monster.blanket_rgb = color(45, 59, 252);
             monster.sleep();
         }
@@ -165,14 +168,12 @@ class Pillow {
 let canvasWidth = 1400;
 let canvasHeight = 1100;
 
-const monster = new BlanketMonster(400, 400);
-const pillow1 = new Pillow(100, 100);
-
-let annoy = 0; //# of clicks on Monster
-let drawAll = 1; // draw all except face
+let monster;
+let pillow1 = new Pillow(100, 100);
 
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
+    monster = new BlanketMonster(400, 400, color(3, 240, 252));
 }
 
 function draw(){
@@ -192,28 +193,28 @@ function draw(){
 function drawFace(){ //pop up
     noStroke();
     fill(0);
-    rect(0, 0, width, height);
+    rect(0, 0, windowWidth, windowHeight);
 
     rectMode(CENTER);
     fill(255, 0, 0);//eye blood
-    rect(width / 2 - 300, 500, 50, 400, 50);
-    rect(width / 2 + 300, 500, 50, 400, 50);
+    rect(windowWidth / 2 - 300, 500, 50, 400, 50);
+    rect(windowWidth / 2 + 300, 500, 50, 400, 50);
     fill(255);//eyes
-    ellipse(width / 2 - 300, 300, 200, 100);
-    ellipse(width / 2 + 300, 300, 200, 100);
+    ellipse(windowWidth / 2 - 300, 300, 200, 100);
+    ellipse(windowWidth / 2 + 300, 300, 200, 100);
 
     fill(255);//mouth
-    ellipse(width / 2, 800, 500, 200);
+    ellipse(windowWidth / 2, 800, 500, 200);
 
     fill(0);//teeth 
-    ellipse(width / 2, 700, 50, 100);
-    ellipse(width / 2 + 50, 900, 50, 100);
+    ellipse(windowWidth / 2, 700, 50, 100);
+    ellipse(windowWidth / 2 + 50, 900, 50, 100);
 
     for (let i = 0; i < 2; i++) {
-        ellipse(width / 2 + 100 * (i + 1), 700, 50, 100);
-        ellipse(width / 2 - 100 * (i + 1), 700, 50, 100);
-        ellipse(width / 2 + 50 + 100 * (i + 1), 900, 50, 100);
-        ellipse(width / 2 + 50 - 100 * (i + 1), 900, 50, 100);
+        ellipse(windowWidth / 2 + 100 * (i + 1), 700, 50, 100);
+        ellipse(windowWidth / 2 - 100 * (i + 1), 700, 50, 100);
+        ellipse(windowWidth / 2 + 50 + 100 * (i + 1), 900, 50, 100);
+        ellipse(windowWidth / 2 + 50 - 100 * (i + 1), 900, 50, 100);
     }
     //reset rectMode
     rectMode(CORNER);
