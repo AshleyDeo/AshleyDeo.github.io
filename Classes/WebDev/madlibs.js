d75c019cd5565624c35cbca31693479f576265ec
AshleyDeo <35739765+AshleyDeo@users.noreply.github.com>
2020-10-07 16:24:13 -0400

    Fix madlibs

--- Classes/WebDev/madlibs.js
+++ Classes/WebDev/madlibs.js
@@ -3,27 +3,20 @@ app.controller('inputCtrl', function ($scope) {
     $scope.game = "Game";
     $scope.plant = "Plant";
     $scope.place = "Place";
-    $scope.num = "Num";
-    $scope.adj = ["A", "B", "C"];
-    $scope.noun = ["A", "B", "C", "D"];
-    $scope.plnoun = ["A", "B", "C"];
-    $scope.verbing = ["A", "B", "C", "D"];
-
-    //$scope.addAdj = function () {
-    //    $scope.contact.adj.push($scope.inpTxt);
-    //    console.log($scope.contact.adj);
-    //};
+    $scope.num = "Number";
+    $scope.adj0 = "Adjective"
+    $scope.adj1 = "Adjective"
+    $scope.adj2 = "Adjective"
+    $scope.noun0 = "Noun"
+    $scope.noun1 = "Noun"
+    $scope.noun2 = "Noun"
+    $scope.noun3 = "Noun"
+    $scope.plnoun0 = "Plural Noun"
+    $scope.plnoun1 = "Plural Noun"
+    $scope.plnoun2 = "Plural Noun"
+    $scope.verbing0 = "Verb ending in -ing"
+    $scope.verbing1 = "Verb ending in -ing"
+    $scope.verbing2 = "Verb ending in -ing"
+    $scope.verbing3 = "Verb ending in -ing"
+    $scope.showStory = false;
 });
-
-// making a div a button
-// displaying to another div
-//$("#target").click(function () {
-//    $("#other").text("Now the txt is this!");
-//})
-// accessing user input
-// setting it as a global variable so you can access it later in the code
-//var theText;
-//$("#theSubmit").click(function () {
-//    theText = $("#theText").val();
-//    console.log(theText);
-//})
